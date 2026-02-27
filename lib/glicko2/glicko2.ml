73aac34a9d2b7aa237396e4b35acd530ca659d0c
Angel ILIEV <a.v.iliev13@gmail.com>
2026-02-27 22:29:29 +0200

    wip: glicko rating for whole race

--- lib/glicko2/glicko2.ml
+++ lib/glicko2/glicko2.ml
@@ -56,6 +56,63 @@ let update_ratings t =
 
 let players t = List.map (Map.data t.players_map) ~f:Player_rating.of_player
 
+let of_race ~(settings : Settings.t) ~(race : Race.Race.t) =
+  let t = create ~settings in
+
+  let all_participants = List.concat race in
+  let t =
+    List.fold all_participants ~init:t ~f:(fun t participant ->
+        match participant.stats with
+        | None -> add_player_with_defaults t ~id:participant.id
+        | Some stats ->
+            add_player t ~id:participant.id ~rating:stats.rating ~rd:stats.rd
+              ~vol:stats.vol)
+  in
+
+  let rec compute_matches (t : t) (participants : Race.Participant.t list)
+      (outcome : Outcome.t) =
+    match participants with
+    | [] -> t
+    | _ :: [] -> t
+    | hd :: tl ->
+        let t =
+          List.fold tl ~init:t ~f:(fun t p ->
+              add_match t ~player1_id:hd.id ~player2_id:p.id ~outcome)
+        in
+        compute_matches t tl outcome
+  in
+
+  let t =
+    List.foldi race ~init:t ~f:(fun i t participants ->
+        (* if there is more than 1 participant on the same place (i.e. same
+           overall time) - make draws between each participant pair *)
+        let t = compute_matches t participants Outcome.Draw in
+
+        (* Get only participants after this position & add all matches 
+           with each participant from this position as a win against the remaining
+           participants of the race that come after this position *)
+        let participants_after_i = List.drop race (i + 1) in
+
+        (* NOTE: good luck to me if i ever have to fix this *)
+        let t =
+          List.fold participants ~init:t ~f:(fun t current_participant ->
+              List.fold participants_after_i ~init:t
+                ~f:(fun t participants_after ->
+                  List.fold participants_after ~init:t
+                    ~f:(fun t participant_after ->
+                      add_match t ~player1_id:current_participant.id
+                        ~player2_id:participant_after.id
+                        ~outcome:Outcome.Victory)))
+        in
+        t)
+  in
+
+  t
+
+(* TODO: store rating information for each player for each race (along with
+   rating change & new values for rd and vol *)
+(* TODO: Test on real data what is meaningfull default values for tau, rd, vol etc. *)
+
 let%expect_test "update_ratings_1" =
   let settings =
     Settings.create ~tau:0.5 ~rd:200. ~vol:0.06 ~initial_rating:1500. ()
@@ -83,14 +140,7 @@ let%expect_test "update_ratings_1" =
     { id = 3; rating = 1570.39474024; rd = 97.709168522; vol = 0.059999419472 }
     { id = 4; rating = 1784.42179013; rd = 251.565564532; vol = 0.0599990117637 } |}]
 
-(* TODO: add support for full race and the results from it should be the same as this 
-        [
-          [Ryan], //Ryan won the race
-          [Bob, John], //Bob and John 2nd position ex-aequo
-          [Mary] // Mary 4th position
-        ]
- *)
-let%expect_test "update_ratings_1" =
+let%expect_test "update_ratings_2" =
   let settings =
     Settings.create ~tau:0.5 ~rd:200. ~vol:0.06 ~initial_rating:1500. ()
   in
@@ -120,3 +170,37 @@ let%expect_test "update_ratings_1" =
     { id = 2; rating = 1399.22100492; rd = 31.5691156995; vol = 0.0599954322606 }
     { id = 3; rating = 1539.88476704; rd = 93.0270600887; vol = 0.0599946096036 }
     { id = 4; rating = 1369.19439435; rd = 212.313018397; vol = 0.0600032317461 } |}]
+
+let%expect_test "of_race" =
+  let settings =
+    Settings.create ~tau:0.5 ~rd:200. ~vol:0.06 ~initial_rating:1500. ()
+  in
+  (* TODO: double check each players opponents and outcomes in the Player.t object 
+    to make sure the whole algorihtm added matches correctly *)
+  let race =
+    [
+      [ Race.Participant.Fields.create ~id:1 ~stats:None ];
+      [
+        Race.Participant.Fields.create ~id:2
+          ~stats:
+            (Some (Race.Stats.Fields.create ~rating:1400. ~rd:30. ~vol:0.06));
+        Race.Participant.Fields.create ~id:3
+          ~stats:
+            (Some (Race.Stats.Fields.create ~rating:1550. ~rd:100. ~vol:0.06));
+      ];
+      [
+        Race.Participant.Fields.create ~id:4
+          ~stats:
+            (Some (Race.Stats.Fields.create ~rating:1700. ~rd:300. ~vol:0.06));
+      ];
+    ]
+  in
+  let glicko = of_race ~settings ~race |> update_ratings in
+  let players = players glicko in
+  List.iter players ~f:(fun p -> printf "%s\n" (Player_rating.show p));
+  [%expect
+    {|
+    { id = 1; rating = 1685.72145039; rd = 151.516557043; vol = 0.0600043923961 }
+    { id = 2; rating = 1402.44380033; rd = 31.5219636222; vol = 0.0599963879198 }
+    { id = 3; rating = 1539.88476704; rd = 93.0270600887; vol = 0.0599946096036 }
+    { id = 4; rating = 1238.2341119; rd = 194.563191646; vol = 0.060008079619 } |}]
