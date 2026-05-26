d8a7991243e443e334fe3087f47a8356e3562db0
Angel ILIEV <a.v.iliev13@gmail.com>
2026-05-26 10:24:49 +0300

    wip: prev-next events div

--- lib/web/index.ml
+++ lib/web/index.ml
@@ -13,6 +13,71 @@ let head_elems (t : Page_settings.t) =
         ];
     ]
 
+let ratings_table (t : Page_settings.t) =
+  let _ = t in
+  null
+    [
+      div
+        [ class_ "box__top" ]
+        [
+          (* TODO: put filter buttons here *)
+          a
+            [ path_attr href Paths.user_w_scope t.translations.lang ]
+            [ txt "User" ];
+        ];
+      table
+        [ class_ "slist slist-pad slist-invert slist-leaderboard" ]
+        [
+          tbody
+            [ class_ "infinite-scroll" ]
+            [
+              tr []
+                [
+                  td []
+                    [
+                      span
+                        [
+                          class_ "trophy perf top1 lb__trophy trophy--small";
+                          title_ "Blitz Chamption!";
+                        ]
+                        [
+                          img
+                            [
+                              src
+                                "https://lichess1.org/assets/hashed/gold-cup-2.e1e2ac3f.png";
+                              style_ "height: 40px;";
+                            ];
+                        ];
+                    ];
+                  td []
+                    [
+                      a
+                        [
+                          class_ "offline user-link ulpt";
+                          path_attr href Paths.index;
+                        ]
+                        [ txt "asdert9" ];
+                    ];
+                  td [] [ txt "3043" ];
+                  td [] [ txt "10" ];
+                ];
+            ];
+        ];
+    ]
+
+let prev_next_event (t : Page_settings.t) =
+  let _ = t in
+  div
+    [ class_ "prev-next-events" ]
+    [
+      div
+        [ class_ "page-small box" ]
+        [
+          div [ class_ "prev-event" ] [ txt "Past Event" ];
+          div [ class_ "next-event" ] [ txt "Next Event" ];
+        ];
+    ]
+
 let page (t : Page_settings.t) =
   html
     [ lang "en" ]
@@ -24,60 +89,8 @@ let page (t : Page_settings.t) =
           div
             [ id "main-wrap" ]
             [
-              main
-                [ class_ "page-small box" ]
-                [
-                  div
-                    [ class_ "box__top" ]
-                    [
-                      (* TODO: put filter buttons here *)
-                      a
-                        [
-                          path_attr href Paths.user_w_scope t.translations.lang;
-                        ]
-                        [ txt "User" ];
-                    ];
-                  table
-                    [ class_ "slist slist-pad slist-invert slist-leaderboard" ]
-                    [
-                      tbody
-                        [ class_ "infinite-scroll" ]
-                        [
-                          tr []
-                            [
-                              td []
-                                [
-                                  span
-                                    [
-                                      class_
-                                        "trophy perf top1 lb__trophy \
-                                         trophy--small";
-                                      title_ "Blitz Chamption!";
-                                    ]
-                                    [
-                                      img
-                                        [
-                                          src
-                                            "https://lichess1.org/assets/hashed/gold-cup-2.e1e2ac3f.png";
-                                          style_ "height: 40px;";
-                                        ];
-                                    ];
-                                ];
-                              td []
-                                [
-                                  a
-                                    [
-                                      class_ "offline user-link ulpt";
-                                      path_attr href Paths.index;
-                                    ]
-                                    [ txt "asdert9" ];
-                                ];
-                              td [] [ txt "3043" ];
-                              td [] [ txt "10" ];
-                            ];
-                        ];
-                    ];
-                ];
+              prev_next_event t;
+              main [ class_ "page-small box" ] [ ratings_table t ];
             ];
         ];
     ]
